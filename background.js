5d6bcde418a262c011265508265febf0ba2b5189
samadova.minira <minira.samadova@syncthought.com>
2019-06-28 15:39:48 +0900

    added tab tracking

--- background.js
+++ background.js
@@ -1,23 +1,11 @@
 let websites = {};
+let currentlyTrackedDomain = null;
 
-//listen for messages from popup
+
+//listen for messages
 chrome.runtime.onMessage.addListener(onMessage);
 
-function getDomainFromUrl(url) {
-  const matches = url.match(/^(https?:\/\/[^\/]+)/);
-  return matches[1];
-}
-
-function handleStartTracking(message) {
-  if (websites[message.url] === undefined) {
-    websites[message.url ] = {
-      totalTime: 0
-    };
-  } else {
-    alert("You are already tracking this website");
-  }
-}
-
+//the one who desides what to do next depending on message
 function onMessage(message, sender, sendResponse) {
   switch (message.type) {
     case 'START_TRACKING': {
@@ -28,14 +16,44 @@ function onMessage(message, sender, sendResponse) {
 
 };
 
-chrome.tabs.onActivated.addListener(whoIsActive);
+//get the domain from URL
+function getDomainFromUrl(url) {
+  const matches = url.match(/^(https?:\/\/[^\/]+)/);
+  return matches[1];
+}
 
-//let's track the active id
-function whoIsActive(activeInfo) {
-  let time = new Date();
-  console.log(activeInfo, time);
-  if (websites[activeInfo.id] !== undefined) {
+//execute in case "track" button was clicked
+function handleStartTracking(message) {
 
+  let domain = getDomainFromUrl(message.url);
+  currentlyTrackedDomain = domain;
+
+  if (websites[domain] === undefined) {
+    currentlyTrackedDomain = domain;
+    websites[domain] = new Date();
+  } else {
+    alert("You are already tracking this website");
   }
 }
 
+chrome.tabs.onActivated.addListener(activeTabChange);
+
+//take actions in case acrtive tab is changed
+function activeTabChange(activeInfo) {
+  if (currentlyTrackedDomain !== null) {
+    let currentTime = new Date();
+    websites[currentlyTrackedDomain] = Math.floor((currentTime - websites[currentlyTrackedDomain]) / 1000);
+    //we moved from the tracking tab, so we need to clear the value
+    currentlyTrackedDomain = null;
+  };
+
+  chrome.tabs.get(activeInfo.tabId, getCurrentTabUrl);
+
+  //if we open a tab which we already track
+  function getCurrentTabUrl(tab) {
+    if (websites[tab.url] !== undefined) {
+      currentlyTrackedDomain = websites[tab.url];
+    };
+
+  };
+}
--- popup.js
+++ popup.js
@@ -1,24 +1,24 @@
 window.addEventListener('load', function load(event) {
-      //create event listener for Track button
-      var createButton = document.getElementById('create_button');
+  //create event listener for Track button
+  var createButton = document.getElementById('create_button');
 
-      createButton.addEventListener('click', function() {
-          let params = {
-            active: true
-          };
+  createButton.addEventListener('click', function() {
+    let params = {
+      active: true
+    };
 
-          //trigger the following function each time button is clicked
+    //trigger the following function each time button is clicked
 
-          chrome.tabs.query(params, getCurrent);
+    chrome.tabs.query(params, getCurrent);
 
-          //send current tab url to background script
-          function getCurrent(tabs) {
-            let message = {
-              type: 'START_TRACKING',
-              url: tabs[0].url
-              // id: tabs[0].id
-            }
-            chrome.runtime.sendMessage(message);
-          };
-      });
-    });
+    //send current tab url to background script
+    function getCurrent(tabs) {
+      let message = {
+        type: 'START_TRACKING',
+        url: tabs[0].url
+        // id: tabs[0].id
+      }
+      chrome.runtime.sendMessage(message);
+    };
+  });
+});
