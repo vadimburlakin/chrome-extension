e60d209784262e451290d2896f4894b7ae343535
Minira Samadova <samadova.minira@gmail.com>
2019-07-23 18:52:10 +0900

    added jest

---
+++ momentJsImitation.js
@@ -0,0 +1,25 @@
+function showRightTime(time) {
+  if (time < 60) {
+    return `${time} seconds`;
+  } else if (time > 59 && time < 3600) {
+    time /= 60;
+    let minutes = Math.floor(time);
+    let seconds = (time - minutes) * 60;
+    switch (seconds) {
+      case 0:
+        return `${minutes} minutes`;
+      default:
+        return `${minutes} minutes ${seconds} seconds`
+    }
+  } else if (time > 3599) {
+    time = Math.floor(time / 60);
+    let hours = Math.floor(time / 60);
+    let minutes = time % 60;
+    switch (minutes) {
+      case 0:
+        return `${hours} hours`;
+      default:
+        return `${hours} hours ${minutes} minutes`;
+    }
+  }
+}
