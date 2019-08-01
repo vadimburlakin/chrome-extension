ab3e64c5293c0ad917281c3f44f3f735aa3da80b
samadova.minira <minira.samadova@syncthought.com>
2019-08-01 15:31:10 +0900

    edited function to switch between plural and singular words

--- momentJsImitation.js
+++ momentJsImitation.js
@@ -1,28 +1,71 @@
-const editTime = (time) => {
+const timeTypes = {
+  "hours": "hours",
+  "hour": "hour",
+  "minutes": "minutes",
+  "minute": "minute",
+  "seconds": "seconds",
+  "second": "second"
+};
+
+const editTime = (time, timeTypes) => {
+  let result = null;
   if (time < 60) {
-    return `${time} seconds`;
+    switch (time) {
+      case 1:
+      result = `${time} ${timeTypes.second}`;
+      break;
+      default:
+      result = `${time} ${timeTypes.seconds}`;
+      break;
+    }
   } else if (time > 59 && time < 3600) {
     let minutes = Math.floor(time / 60);
     let seconds = time - (Math.floor(time / 60) * 60);
+    switch (minutes) {
+      case 1:
+      result = `${minutes} ${timeTypes.minute}`;
+      break;
+      default:
+      result = `${minutes} ${timeTypes.minutes}`;
+      break;
+    }
     switch (seconds) {
       case 0:
-        return `${minutes} minutes`;
+        break;
       case 1:
-        return `${minutes} minutes ${seconds} second`;
+        result += ` ${seconds} ${timeTypes.second}`;
+        break;
       default:
-        return `${minutes} minutes ${seconds} seconds`;
+        result += ` ${seconds} ${timeTypes.seconds}`;
+        break;
     }
   } else if (time > 3599) {
     time = Math.floor(time / 60);
     let hours = Math.floor(time / 60);
     let minutes = time % 60;
+    switch (hours) {
+      case 0:
+        break;
+      case 1:
+        result = `${hours} ${timeTypes.hour}`;
+        break;
+      default:
+        result = `${hours} ${timeTypes.hours}`;
+        break;
+    }
     switch (minutes) {
       case 0:
-        return `${hours} hours`;
+      break;
+      case 1:
+      result += ` ${minutes} ${timeTypes.minute}`;
+      break;
       default:
-        return `${hours} hours ${minutes} minutes`;
+      result += ` ${minutes} ${timeTypes.minutes}`;
+      break;
     }
   }
+  return result;
 }
 
-module.exports = editTime;
+module.exports.editTime = editTime;
+module.exports.timeTypes = timeTypes;
--- momentJsImitation.test.js
+++ momentJsImitation.test.js
@@ -1,9 +1,12 @@
-const editTime = require('./momentJsImitation');
+const editTime = require('./momentJsImitation').editTime;
+const timeTypes = require('./momentJsImitation').timeTypes;
+
 
 test('test for a test', () => {
-  expect(editTime(120)).toBe('2 minutes');
-  expect(editTime(54)).toBe('54 seconds');
-  expect(editTime(129)).toBe('2 minutes 9 seconds');
-  expect(editTime(3600)).toBe('1 hours');
-  expect(editTime(0)).toBe('0 seconds');
+  expect(editTime(120, timeTypes)).toBe('2 minutes');
+  expect(editTime(54, timeTypes)).toBe('54 seconds');
+  expect(editTime(129, timeTypes)).toBe('2 minutes 9 seconds');
+  expect(editTime(3600, timeTypes)).toBe('1 hour');
+  expect(editTime(0, timeTypes)).toBe('0 seconds');
+  expect(editTime(60, timeTypes)).toBe('1 minute');
 });
