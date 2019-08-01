bf05f09eddf77bb50807a3ce479e9a60fe0ea192
samadova.minira <minira.samadova@syncthought.com>
2019-08-01 17:06:00 +0900

    trying to fix lint

--- momentJsImitation.js
+++ momentJsImitation.js
@@ -11,10 +11,10 @@ const editTime = (time, timeTypes) => {
   let result = null;
   if (time < 60) {
     switch (time) {
-      case 1:
+    case 1:
       result = `${time} ${timeTypes.second}`;
       break;
-      default:
+    default:
       result = `${time} ${timeTypes.seconds}`;
       break;
     }
@@ -22,50 +22,50 @@ const editTime = (time, timeTypes) => {
     let minutes = Math.floor(time / 60);
     let seconds = time - (Math.floor(time / 60) * 60);
     switch (minutes) {
-      case 1:
+    case 1:
       result = `${minutes} ${timeTypes.minute}`;
       break;
-      default:
+    default:
       result = `${minutes} ${timeTypes.minutes}`;
       break;
     }
     switch (seconds) {
-      case 0:
-        break;
-      case 1:
-        result += ` ${seconds} ${timeTypes.second}`;
-        break;
-      default:
-        result += ` ${seconds} ${timeTypes.seconds}`;
-        break;
+    case 0:
+      break;
+    case 1:
+      result += ` ${seconds} ${timeTypes.second}`;
+      break;
+    default:
+      result += ` ${seconds} ${timeTypes.seconds}`;
+      break;
     }
   } else if (time > 3599) {
     time = Math.floor(time / 60);
     let hours = Math.floor(time / 60);
     let minutes = time % 60;
     switch (hours) {
-      case 0:
-        break;
-      case 1:
-        result = `${hours} ${timeTypes.hour}`;
-        break;
-      default:
-        result = `${hours} ${timeTypes.hours}`;
-        break;
+    case 0:
+      break;
+    case 1:
+      result = `${hours} ${timeTypes.hour}`;
+      break;
+    default:
+      result = `${hours} ${timeTypes.hours}`;
+      break;
     }
     switch (minutes) {
-      case 0:
+    case 0:
       break;
-      case 1:
+    case 1:
       result += ` ${minutes} ${timeTypes.minute}`;
       break;
-      default:
+    default:
       result += ` ${minutes} ${timeTypes.minutes}`;
       break;
     }
   }
   return result;
-}
+};
 
 module.exports.editTime = editTime;
 module.exports.timeTypes = timeTypes;
--- momentJsImitation.test.js
+++ momentJsImitation.test.js
@@ -1,12 +1,12 @@
-const editTime = require('./momentJsImitation').editTime;
-const timeTypes = require('./momentJsImitation').timeTypes;
+const editTime = require("./momentJsImitation").editTime;
+const timeTypes = require("./momentJsImitation").timeTypes;
 
 
-test('test for a test', () => {
-  expect(editTime(120, timeTypes)).toBe('2 minutes');
-  expect(editTime(54, timeTypes)).toBe('54 seconds');
-  expect(editTime(129, timeTypes)).toBe('2 minutes 9 seconds');
-  expect(editTime(3600, timeTypes)).toBe('1 hour');
-  expect(editTime(0, timeTypes)).toBe('0 seconds');
-  expect(editTime(60, timeTypes)).toBe('1 minute');
+test("test for a test", () => {
+  expect(editTime(120, timeTypes)).toBe("2 minutes");
+  expect(editTime(54, timeTypes)).toBe("54 seconds");
+  expect(editTime(129, timeTypes)).toBe("2 minutes 9 seconds");
+  expect(editTime(3600, timeTypes)).toBe("1 hour");
+  expect(editTime(0, timeTypes)).toBe("0 seconds");
+  expect(editTime(60, timeTypes)).toBe("1 minute");
 });
